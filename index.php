bb88cd0df2c1a5c1acb25f7e2bf60c98eaffd4ea
Josh Cunningham <josh@joshcanhelp.com>
2016-01-20 09:59:18 -0800

    basic page structure for main index.php template

--- index.php
+++ index.php
@@ -1,36 +1,42 @@
 <?php get_header(); ?>
 
-<?php
-if ( have_posts() ) :
-	while ( have_posts() ) :
-		the_post();
-		?>
+	<section class="single-page">
+		<div class="inner">
 
-		<article id="post-id-<?php  the_ID(); ?>" <?php post_class( 'u-clear' ); ?>>
+			<?php
+			if ( have_posts() ) :
+				while ( have_posts() ) :
+					the_post();
+					?>
 
-			<img src="<?php echo allonsy_get_post_img_url() ?>" class="alignleft">
+					<article id="post-id-<?php the_ID(); ?>" <?php post_class( 'u-clear' ); ?>>
 
-			<?php if ( is_singular() ) : ?>
-			<h1><?php the_title(); ?></h1>
-			<?php else : ?>
-			<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-			<?php endif; ?>
+						<img src="<?php echo allonsy_get_post_img_url() ?>" class="alignleft">
 
-			<div class="has-wysiwyg">
+						<?php if ( is_singular() ) : ?>
+							<h1><?php the_title(); ?></h1>
+						<?php else : ?>
+							<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+						<?php endif; ?>
 
-				<?php if ( is_singular() ) : ?>
-					<?php the_content(); ?>
-				<?php else : ?>
-					<?php the_excerpt(); ?>
-				<?php endif; ?>
+						<div class="has-wysiwyg">
 
-			</div>
-		</article>
+							<?php if ( is_singular() ) : ?>
+								<?php the_content(); ?>
+							<?php else : ?>
+								<?php the_excerpt(); ?>
+							<?php endif; ?>
 
-	<?php
-	endwhile;
-endif;
-?>
+						</div>
+					</article>
+
+					<?php
+				endwhile;
+			endif;
+			?>
+			
+		</div>
+	</section>
 
 
 <?php get_footer(); ?>
