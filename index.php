0476963df5302cc61583530a12ef970724cdc156
Josh Cunningham <josh@joshcanhelp.com>
2017-05-11 10:51:59 -0700

    Update index.php

--- index.php
+++ index.php
@@ -8,17 +8,17 @@ if ( have_posts() ) :
 
         <article id="post-id-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-		    <?php if ( is_singular() ) : ?>
-                <h1><?php the_title(); ?></h1>
-		    <?php else : ?>
+	<?php if ( is_singular() ) : ?>
+      		<h1><?php the_title(); ?></h1>
+	<?php else : ?>
                 <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-		    <?php endif; ?>
+	<?php endif; ?>
 
-			<?php if ( is_singular() ) : ?>
-				<?php the_content(); ?>
-			<?php else : ?>
-				<?php the_excerpt(); ?>
-			<?php endif; ?>
+	<?php if ( is_singular() ) : ?>
+		<?php the_content(); ?>
+	<?php else : ?>
+		<?php the_excerpt(); ?>
+	<?php endif; ?>
 
         </article>
 
